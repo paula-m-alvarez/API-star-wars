2603f6a60a28469fe3176ec0af0440c34b857fb0
Pau Alvarez <alvarez.paula.maria@gmail.com>
2023-08-20 22:13:20 -0300

    Added new category call - Info about the characters

--- index.js
+++ index.js
@@ -3,17 +3,46 @@ const axios = require("axios");
 
 const app = express();
 
+const api = axios.create({
+    baseURL: "https://swapi.dev/api"
+})
 
-app.get("/", async (req, res) => {
-    const result = await api.get("/people/1")
-    res.send("<html><body><h1> Star Wars Api</h1></body></html>");
+
+app.get("/:category/:id", async (req, res) => {
+    const { category, id } = req.params;
+
+    try {
+        let { data } = await api.get(`/${category}/${id}`);
+        console.log(data);
+
+        await Promise.all(
+            Object.entries(data).map(async (p) => {
+                if(p[0] == "homeworld") {
+                    const homeworld = await api.get(
+                        `/planets/${p[1].split(`planets/`)[1]}`
+                    );
+                    data[p[0]] = homeworld.data.name;
+                };
+            })
+        );
+
+        const result = `
+        <html>
+            <body>
+                <h1> Name of character: ${data.name} </h1>
+                <p> Height: ${data.height} </p>
+                <p> Mass: ${data.mass} </p>
+                <p> Homeworld: ${data.homeworld} </p>
+            </body>
+        </html>
+        `;
+        res.send(result);
+    } catch(error) {
+        res.send("<html><body><h1>Error</h1></body></html>")
+    }
+
 });
 
 app.listen(3000, () => {
-    console.log("Servidor Iniciado")
+    console.log("Server started")
 });
-
-const api = axios.create({
-    baseUrl: "https://swapi.dev/api"
-})
-
